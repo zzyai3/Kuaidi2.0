dbdc780da1b8af0eb1d048ce9d8bfb46096f1592
zhouzhongyi <695182737@qq.com>
2016-02-17 23:19:59 +0800

    Rejected commit: Default

--- app/src/main/java/com/example/zhouzhongyi/kuaidi/activity/RegisterActicity.java
+++ app/src/main/java/com/example/zhouzhongyi/kuaidi/activity/RegisterActicity.java
@@ -7,6 +7,8 @@ import android.view.View;
 import android.view.Window;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 import com.example.zhouzhongyi.kuaidi.R;
 
@@ -22,6 +24,9 @@ public class RegisterActicity extends Activity implements View.OnClickListener {
     private EditText et_yanzheng;
     private Button bt_register;
     private Button bt_yanzhengma;
+    private RadioGroup rg;
+    private RadioButton r_shopping_button;
+    private RadioButton r_song_button;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);//去除原来标题栏
@@ -38,6 +43,9 @@ public class RegisterActicity extends Activity implements View.OnClickListener {
         et_yanzheng=(EditText)findViewById(R.id.et_register_yanzheng);
         bt_yanzhengma=(Button)findViewById(R.id.bt_register_yanzhengma);
         bt_register=(Button)findViewById(R.id.bt_regist_regist);
+        rg=(RadioGroup)findViewById(R.id.rg);
+        r_shopping_button=(RadioButton)findViewById(R.id.r_shopping_button);
+        r_song_button=(RadioButton)findViewById(R.id.r_song_button);
     }
     private void init(){
         bt_register.setOnClickListener(this);
@@ -47,6 +55,10 @@ public class RegisterActicity extends Activity implements View.OnClickListener {
 
     @Override
     public void onClick(View v) {
-
+        String phone = et_phonenum.getText().toString();
+        //初始化
+        cn.smssdk.SMSSDK.initSDK(this, "f82970592038", "e1a79f6f1320a0fbe2c7cc04f068d8d3");
+        //发送短信
+        cn.smssdk.SMSSDK.getVerificationCode("86",phone);
     }
 }
